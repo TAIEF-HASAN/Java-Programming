f39f6358402ad511b819c0c353fa82614b963da7
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-02 14:25:17 +0600

    added new code in test.java

--- Test_Project/src/main/java/com/mycompany/test_project/TestPurpose.java
+++ Test_Project/src/main/java/com/mycompany/test_project/TestPurpose.java
@@ -1,16 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template 
  */
-
 package com.mycompany.test_project;
 
 /**
  *
  * @author PIYAL
  */
-public class Test_Project {
-
+public class TestPurpose {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
+        System.out.print("This is new Project");
+        
     }
 }
