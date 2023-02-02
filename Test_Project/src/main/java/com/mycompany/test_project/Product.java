f6526df6388922dc79faeca7990cf0f8789e9aff
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-02 18:07:44 +0600

    Test 10 Done

---
+++ Test_Project/src/main/java/com/mycompany/test_project/Product.java
@@ -0,0 +1,20 @@
+package com.mycompany.test_project;
+
+public class Product {
+    public static void main(String[] args) {
+        int id= 101;
+        String title = "iphone15";
+        int price = 1895 ;
+        String description = "perfect product with best image quality";
+        String catagory = "phone";
+
+        System.out.println("ID: "+id);
+        System.out.println("Title: "+title);
+        System.out.println(price+" euros");
+        System.out.println("description: "+description);
+        System.out.println("catagory: "+catagory);
+
+
+
+    }
+}
