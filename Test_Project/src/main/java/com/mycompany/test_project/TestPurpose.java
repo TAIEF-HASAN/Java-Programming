2450171e25abebc51e0197f79c69baf2aabae20b
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-05 09:33:27 +0600

    added formet specifier

--- Test_Project/src/main/java/com/mycompany/test_project/TestPurpose.java
+++ Test_Project/src/main/java/com/mycompany/test_project/TestPurpose.java
@@ -20,12 +20,12 @@ public class TestPurpose {
         float f = 10.2f;
         double d = 10.2;
 
-        System.out.println("b ="+ b);
-        System.out.println("c = "+c);
-        System.out.println("s = "+s);
-        System.out.println("i = "+i);
-        System.out.println("f = "+f);
-        System.out.println("d = "+d);
+        System.out.printf("boolean b = %b\n",b);
+        System.out.printf("character c = %c\n",c);
+        System.out.printf("short s = %d\n",s);
+        System.out.printf("integer i = %d\n",i);
+        System.out.printf("float f = %.2f\n",f);
+        System.out.printf("double d = %.3f\n",d);
         
     }
 }
