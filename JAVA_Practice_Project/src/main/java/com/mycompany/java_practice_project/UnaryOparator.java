ebefc427a66fb13db83c37632deb17f27d54b8b3
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-05 16:56:08 +0600

    example of UnaryOparator

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/UnaryOparator.java
@@ -0,0 +1,17 @@
+package com.mycompany.java_practice_project;
+
+public class UnaryOparator {
+    
+    public static void main(String[] args) {
+        
+        int x = 10;
+        int y;
+
+        y = +x;
+        System.out.println("Y "+y);
+
+        y = -x;
+        System.out.println("y :"+y);
+
+    }
+}
