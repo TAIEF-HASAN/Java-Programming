64ccef30e4e60cafa81971dd048e099f14461293
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-06 10:03:41 +0600

    assignment6 complete

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/CheckingVowelConsonent.java
@@ -0,0 +1,34 @@
+package com.mycompany.java_practice_project;
+
+import java.util.Scanner;
+
+public class CheckingVowelConsonent {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        String letter;
+
+        System.out.print("Enter Letter and check if vowel or consonant:");
+        letter = input.next();
+
+        if(letter=="a"){
+            System.out.println("vowel");
+        }
+        else if(letter=="e"){
+            System.out.println("vowel");
+        }
+        else if(letter=="i"){
+            System.out.println("vowel");
+        }
+        else if(letter=="o"){
+            System.out.println("vowel");
+        }
+        else if(letter=="u"){
+            System.out.println("vowel");
+        }
+        else{
+            System.out.println("consonent");
+        }
+
+    }
+}
