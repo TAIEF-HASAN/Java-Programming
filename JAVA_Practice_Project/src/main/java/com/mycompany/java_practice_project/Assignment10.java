690309d45d52c65d4930d74a8e76fe8ea7c6ba0d
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-06 17:32:13 +0600

    sum of 1-10 using for loop

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/Assignment10.java
@@ -0,0 +1,17 @@
+package com.mycompany.java_practice_project;
+
+public class Assignment10 {
+    
+    public static void main(String[] args) {
+        
+        int i;
+        int sum = 0;
+
+        for(i=1; i<=10; i++){
+
+            sum = sum + i;
+
+        }
+        System.out.println("sum of 1-10: "+sum);
+    }
+}
