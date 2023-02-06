ab358f5f325eafed28971dd0ffb19440a5814b49
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-06 15:45:41 +0600

    for loop example

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/ForLoopExp.java
@@ -0,0 +1,15 @@
+package com.mycompany.java_practice_project;
+
+public class ForLoopExp {
+    
+    public static void main(String[] args) {
+        
+        for(int i=2; i<=100; i= i+2){
+            System.out.println(i);
+        }
+        
+        for(int i=10; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}
