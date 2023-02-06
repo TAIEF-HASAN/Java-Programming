6336a951f45c4f8e4dd1db99f949d30d7be03643
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-06 16:13:57 +0600

    WhileLoop Example

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/ForLoopExp.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/ForLoopExp.java
@@ -11,5 +11,9 @@ public class ForLoopExp {
         for(int i=10; i>=1; i--){
             System.out.println(i);
         }
+
+        for(int i=1; i<=10; ++i){
+            System.out.println(i);
+        }
     }
 }
---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/WhileLoopExp.java
@@ -0,0 +1,32 @@
+package com.mycompany.java_practice_project;
+
+public class WhileLoopExp {
+    
+    public static void main(String[] args) {
+        
+        int i = 1;
+
+        while(i<=10){
+            System.out.println(i+"Bangladesh");
+            i++;
+        }
+
+        int j = 10;
+        while(j>=1){
+            System.out.println(j);
+            j--;
+        }
+
+        int n = 2;
+        while(n<=10){
+            System.out.println(n);
+            n = n +2 ;
+        }
+
+        int m = 1;
+        while(m<=10){
+            System.out.println(m);
+            ++m;
+        }
+    }
+}
