d119abbf0ba0d2629fe73141316b51e452f46bfd
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-07 14:29:25 +0600

    multiple time table Example

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/TimeTable.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/TimeTable.java
@@ -7,15 +7,22 @@ public class TimeTable {
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
-        int n, result;
+        int n, m,result;
 
         System.out.println("Which number of time table you want to see? please enter the number");
         n = input.nextInt();
+        m = input.nextInt();
 
-        for(int i = 1; i<=10; i++){
+        
 
-            result = i * n;
-            System.out.println(i+" Time "+n+" = "+ result );
+        for(int i = n; i<=m; i++){
+
+            for(int j = 1; j<=10; j++){
+
+                result = j * n;
+                System.out.println(j+" Time "+i+" = "+ result );
+            }
+            System.out.println();
         }
     }
 }
