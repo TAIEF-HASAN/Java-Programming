1e4300b2b3453c68feec26e8599c23fec9d27fba
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-07 12:37:16 +0600

    modified

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/Assignment11.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/Assignment11.java
@@ -8,16 +8,25 @@ public class Assignment11 {
         
 
         Scanner input  = new Scanner(System.in);
-        int fact=1, i=1, n;
+        int fact=1, fact2 = 1, i=1, n;
         System.out.print("Enter a Number for calculate the factorial:");
         n = input.nextInt();
 
+        //a way to define a factorial
         for(i=1; i<=n; i++){
 
             fact = fact * i;
         }
 
         System.out.println("The factorial of "+ n + " is "+fact);
+
+        //another way to define a factorial
+        for(i=n; i>=1; i--){
+
+            fact2 = fact2 * i;
+        }
+
+        System.out.println("The factorial of "+ n + " is "+fact2);
 
     }
 }
