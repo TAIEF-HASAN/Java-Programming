f731e2b538329ddab5c00470b1aa8d7e087aae68
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-07 16:53:31 +0600

    Modified

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/PrimeNumber.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/PrimeNumber.java
@@ -12,20 +12,23 @@ public class PrimeNumber {
         System.out.println("Enter the number to check if the number is prime or not!");
         n = input.nextInt();
 
-        for(int i=2; i<n; i++){
+        if(n==0 || n==1){
+            System.out.println("not a prime");
+        }else{
+            for(int i=2; i<n; i++){
 
-            if(n%i==0){
-                flag = true;
-                break;
+                if(n%i==0){
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag==false){
+                System.out.println("The Number is Prime");
+            }
+            else{
+                System.out.println("The Number is not Prime");
             }
         }
-        if(flag==false){
-            System.out.println("The Number is Prime");
-        }
-        else{
-            System.out.println("The Number is not Prime");
-        }
-        
 
     }
 }
