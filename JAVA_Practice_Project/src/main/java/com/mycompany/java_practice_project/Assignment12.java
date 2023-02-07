54dbc84041ba3a125e319f81cfcd1d1452a157d0
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-07 10:52:33 +0600

    Example sum of odd numbers from m-n

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/Assignment12.java
@@ -0,0 +1,41 @@
+package com.mycompany.java_practice_project;
+
+import java.util.Scanner;
+
+public class Assignment12 {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int m,n, sum = 0;
+
+        System.out.print("Enter the number between m-n for result of odd number:");
+        m = input.nextInt();
+        n = input.nextInt();
+
+
+        if(m>n){
+
+            for(int i = m; i>=n; i--){
+
+                if(i %2 !=0){
+                    sum = sum + i;
+                }
+            } 
+        }
+        else{
+
+            for(int i = m; i<=n; i++){
+
+                if(i %2 !=0){
+                    sum = sum + i;
+                }
+            }
+
+        }
+
+        
+
+        System.out.println(sum);
+    }
+}
