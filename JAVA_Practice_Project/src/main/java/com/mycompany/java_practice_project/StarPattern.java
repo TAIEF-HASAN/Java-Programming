ba6596a371505f8608da3a26bbcfbf19f8128533
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-09 17:47:47 +0600

    edded example of StarPattern

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/StarPattern.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/StarPattern.java
@@ -1,7 +1,27 @@
 package com.mycompany.java_practice_project;
 
+import java.util.Scanner;
+
 public class StarPattern {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Pattern numbr:");
+        int n = input.nextInt();
+
+        for(int row=1; row<=n; row++){
+            for(int colm=1; colm<=row; colm++){
+                System.out.print(" *");
+            }
+            System.out.println();
+
+        }
+
+        for(int row=n-1; row>=1; row--){
+            for(int colm=1; colm<=row; colm++){
+                System.out.print(" *");
+            }
+            System.out.println();
+
+        }
     }
 }
