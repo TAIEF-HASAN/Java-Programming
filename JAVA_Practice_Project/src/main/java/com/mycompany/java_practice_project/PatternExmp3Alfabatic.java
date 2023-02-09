6550ce7e3a6430ac288e8f5f3fd97b61641bd9e2
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-09 17:24:30 +0600

    patternExmp1 modified

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/PatternExmp3Alfabatic.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/PatternExmp3Alfabatic.java
@@ -9,7 +9,7 @@ public class PatternExmp3Alfabatic {
         System.out.println("Enter the Pattern Number:");
         int n = input.nextInt();
     
-        for(int i=1; i<=n; i++){
+        for(int i=n; i>=1; i--){
             for(int j=1; j<=i; j++){
                 System.out.printf(" %c",j+64);
             }
