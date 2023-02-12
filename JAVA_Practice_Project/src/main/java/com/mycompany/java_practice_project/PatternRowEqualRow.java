b970cc9323f39c548f969ac0406c49f1519660b1
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-12 15:31:48 +0600

    Example of Pattern Row Equal Column

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/PatternRowEqualRow.java
@@ -0,0 +1,22 @@
+package com.mycompany.java_practice_project;
+
+import java.util.Scanner;
+
+public class PatternRowEqualRow {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        System.out.println("Enter Pattern Number:");
+        n = input.nextInt();
+
+        for(int row=1; row<=n;row++){
+            for(int col=1; col<=n; col++){
+                System.out.printf(" %c",col+96);
+            }
+
+            System.out.println();
+            
+            
+        }
+    }
+}
