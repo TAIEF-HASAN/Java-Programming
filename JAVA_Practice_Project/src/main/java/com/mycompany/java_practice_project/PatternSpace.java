e659f9d721abb0b9e5382d831cedcdf1e2a47d10
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-12 11:22:54 +0600

    Example of Space in  Pattern

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/PatternSpace.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/PatternSpace.java
@@ -1,1 +1,27 @@
 package com.mycompany.java_practice_project;
+
+import java.util.Scanner;
+
+public class PatternSpace {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int n;
+        System.out.print("Enter the patttern number:");
+        n = input.nextInt();
+
+        for(int row=1; row<=n; row++){
+
+            //printing Space
+            for(int col=1; col<=n-row;col++){
+                System.out.print(" ");
+
+            }
+            //Printing Number 
+            for(int col=1; col<=row;col++){
+                System.out.printf("%c",row+64);
+            }
+            System.out.println();
+        }
+    }
+}
