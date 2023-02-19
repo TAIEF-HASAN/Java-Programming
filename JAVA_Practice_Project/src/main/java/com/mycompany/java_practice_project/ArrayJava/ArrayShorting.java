f94bb0b9798c1c7b9231b2de319dda944f11b789
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-19 16:13:04 +0600

    Shorting a Array in Ascending and Descending way

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/ArrayJava/ArrayShorting.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/ArrayJava/ArrayShorting.java
@@ -27,7 +27,7 @@ public class ArrayShorting {
         }
         System.out.println();
         for (int i = number.length-1; i >=0 ; i--) {
-            System.out.print(" "+letter[i]);
+            System.out.print(" "+letter[i]); 
         }
 
 
