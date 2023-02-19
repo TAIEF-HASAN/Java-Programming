f0cf4749e4061be7f51ae84351a4d306e5e453c1
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-19 12:40:08 +0600

    Example of 2-D Array UserInput and output

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/ArrayJava/Array2DInput.java
@@ -0,0 +1,48 @@
+package com.mycompany.java_practice_project.ArrayJava;
+
+import java.util.Scanner;
+
+public class Array2DInput {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int[][] A = new int[2][3];
+        int[][] B = new int[2][3];
+
+        System.out.println("Enter the Value Of A:");
+        for(int row=0; row<2; row++){
+            for (int col = 0; col < 3; col++) {
+                System.out.printf("Put the value of A[%d][%d]:", row,col);
+                A[row][col] = input.nextInt();
+            }
+        }
+
+        System.out.println("Enter the Value Of B:");
+        for(int row=0; row<2; row++){
+            for (int col = 0; col < 3; col++) {
+                System.out.printf("Put the value of B[%d][%d]:", row,col);
+                B[row][col] = input.nextInt();
+            }
+        }
+
+
+
+        System.out.print("A= ");
+        for(int row=0; row<2; row++){
+            for (int col = 0; col < 3; col++) {
+                System.out.print("\t"+A[row][col]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.print("B = ");
+        for(int row=0; row<2; row++){
+            for (int col = 0; col < 3; col++) {
+                System.out.print("\t"+B[row][col]);
+            }
+            System.out.println();
+        }
+
+        
+    }
+}
