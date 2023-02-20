3c47f505c7ce677327899432c0bd2c13b5de59d9
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-20 12:43:09 +0600

    Example of AscAndDesc order of ArrayList

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/ArrayList/AscAndDesc.java
@@ -0,0 +1,30 @@
+package com.mycompany.java_practice_project.ArrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class AscAndDesc {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> number = new ArrayList<>();
+
+        number.add(1);
+        number.add(5);
+        number.add(-11);
+        number.add(-3);
+        number.add(4);
+        number.add(1);
+        number.add(9);
+        number.add(7);
+
+        System.out.println(number);
+
+        //sorted arraylist ascending order
+        Collections.sort(number);
+        System.out.println(number);
+
+        //sorted arraylist descending order
+        Collections.sort(number, Collections.reverseOrder());
+        System.out.println(number);
+    }
+}
