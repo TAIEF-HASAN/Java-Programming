5dba66f4051604737ff1ca5f9fb4687cacf40bfa
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-22 15:08:50 +0600

    modified BitwiseOperator

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/BignnerJava/BitwiseOperator.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/BignnerJava/BitwiseOperator.java
@@ -5,10 +5,10 @@ public class BitwiseOperator {
         
         int a = 32;
         int b = 12;
-
         int c;
-
-        c = a & b;
+        
+        //Bitwise operator do binary Summation between two number and give a result in integer number 
+        c = a & b; 
         System.out.println(c);
 
         c = a | b;
@@ -17,9 +17,11 @@ public class BitwiseOperator {
         c = a ^ b;
         System.out.println(c);
 
+        //left shift do multiply 1 shift left 
         c = a << 3;
         System.out.println(c);
 
+        //right shift do division on shift right
         c = a >> 3;
         System.out.println(c);
     }
