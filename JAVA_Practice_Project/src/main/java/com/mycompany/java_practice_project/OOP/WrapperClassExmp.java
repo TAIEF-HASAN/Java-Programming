a8337a1f787aaed2e5514b7f5718402610bf42b8
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-22 12:52:26 +0600

    Example of WrapperClass

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/WrapperClassExmp.java
@@ -0,0 +1,36 @@
+package com.mycompany.java_practice_project.OOP;
+
+public class WrapperClassExmp {
+    public static void main(String[] args) {
+
+        /*
+         * Wrapper Class:
+         * Auto boxing = Converting primitive to object
+         * Unboxing = Converting object to primitive
+         */
+        
+        //converting primitive to Object of wrapper class
+        int x = 30;
+        Integer y = Integer.valueOf(x);
+        System.out.println("Value of Wrapper class is "+y);
+
+        //We can convert primitive to Object of like this
+        int i = 40;
+        Integer j = i; //here compiler automatically takeoff The wrapper class -->Integer.ValueOf(j)  
+        System.out.println(j);
+
+        //Converting Wrapper Class Object to primitive data type
+        Double a = new Double (4.00);
+        double b = a.doubleValue();
+        System.out.println(b);
+
+        
+        Double k = 3.14;
+        System.out.println(k);
+
+        //we can code like this to convert wrapper class obj to primitive
+        Boolean m = true;
+        boolean n = m; //.booleanValue();
+        System.out.println(n);
+    }
+}
