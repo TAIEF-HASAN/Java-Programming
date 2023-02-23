455fc9404eddf0b3e2c7814cc21b6e63ce8f198e
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-23 16:29:40 +0600

    Changes Directory and File

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/OverloadingConstractor/TeacherInfo2.java
@@ -0,0 +1,34 @@
+package com.mycompany.java_practice_project.OOP.OverloadingConstractor;
+
+public class TeacherInfo2 {
+    
+    String name, gender;
+    int phone;
+
+    //overloading Constructor
+    TeacherInfo2(){
+        System.out.println("No Value");
+    }
+
+    TeacherInfo2(String n, String g){
+        name = n;
+        gender = g;
+    }
+
+    TeacherInfo2(String n, String g, int p){
+        name = n;
+        gender = g;
+        phone = p;
+    }
+
+    void displayInfo(){
+        
+        System.out.println("Name :"+name);
+        System.out.println("Gender :"+gender);
+        System.out.println("Phone :"+phone);
+    }
+
+
+
+
+}
