19b2116009ce5017a870c882a548f80beb542715
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-23 12:38:13 +0600

    modified OOPTest modified TeacherInfo

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/OOPTest.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/OOPTest.java
@@ -17,13 +17,19 @@ public class OOPTest {
         teacher1.gender = "Female";
         teacher1.phone = 17000000;
 
+        
+        teacher.displayInfo();
+        teacher1.displayInfo();
 
-
-        System.out.println("Name :"+teacher.name);
-        System.out.println("Gender :"+teacher.gender);
-        System.out.println("Phone :"+teacher.phone);
-        System.out.println("Name :"+teacher1.name);
-        System.out.println("Gender :"+teacher1.gender);
-        System.out.println("Phone :"+teacher1.phone);
+        /*
+         *  System.out.println("Name :"+teacher.name);
+            System.out.println("Gender :"+teacher.gender);
+            System.out.println("Phone :"+teacher.phone);
+            
+            System.out.println("Name :"+teacher1.name);
+            System.out.println("Gender :"+teacher1.gender);
+            System.out.println("Phone :"+teacher1.phone);
+         */
+      
     }
 }
--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/TeacherInfo.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/TeacherInfo.java
@@ -4,4 +4,16 @@ public class TeacherInfo {
     
     String name, gender;
     int phone;
+
+    /*
+     * methods have many more advantage
+     * we can declare method instead of big line of code to short
+     */
+    void displayInfo(){
+        System.out.println("Name :"+name);
+        System.out.println("Gender :"+gender);
+        System.out.println("Phone :"+phone);
+    }
+
+
 }
