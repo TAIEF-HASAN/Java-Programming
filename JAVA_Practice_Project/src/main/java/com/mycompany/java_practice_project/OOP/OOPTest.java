a0923c7ff4d9354e33860934e115d87324e15ba3
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-23 12:03:05 +0600

    Example of creating class and objects

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/OOPTest.java
@@ -0,0 +1,29 @@
+package com.mycompany.java_practice_project.OOP;
+
+public class OOPTest {
+    public static void main(String[] args) {
+        
+        TeacherInfo teacher; //object declare
+        teacher = new TeacherInfo(); //create object
+
+        //we can create and declare together like this
+        TeacherInfo teacher1 = new TeacherInfo();
+
+        teacher.name = "Rahim";
+        teacher.gender = "male";
+        teacher.phone = 18000000;
+
+        teacher1.name = "Sely";
+        teacher1.gender = "Female";
+        teacher1.phone = 17000000;
+
+
+
+        System.out.println("Name :"+teacher.name);
+        System.out.println("Gender :"+teacher.gender);
+        System.out.println("Phone :"+teacher.phone);
+        System.out.println("Name :"+teacher1.name);
+        System.out.println("Gender :"+teacher1.gender);
+        System.out.println("Phone :"+teacher1.phone);
+    }
+}
---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/TeacherInfo.java
@@ -0,0 +1,7 @@
+package com.mycompany.java_practice_project.OOP;
+
+public class TeacherInfo {
+    
+    String name, gender;
+    int phone;
+}
