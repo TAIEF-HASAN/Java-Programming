f24aa16582dcde4b774e2c556d1e4ae917730a8d
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-23 16:30:17 +0600

    delete file from directory and move another directory

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/RetruningValue/ReturningValuExmp.java
@@ -0,0 +1,8 @@
+package com.mycompany.java_practice_project.OOP.RetruningValue;
+
+public class ReturningValuExmp {
+    
+    int square(int value){
+        return value*value;
+    }
+}
---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/RetruningValue/Test.java
@@ -0,0 +1,10 @@
+package com.mycompany.java_practice_project.OOP.RetruningValue;
+
+public class Test {
+    public static void main(String[] args) {
+        
+        ReturningValuExmp ob1 = new ReturningValuExmp();
+        int result = ob1.square(5);
+        System.out.println(result);
+    }
+}
