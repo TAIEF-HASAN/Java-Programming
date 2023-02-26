9100236067e61633e87ecb76e889f27ef03b1fa3
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-26 09:36:27 +0600

    Difference between methods and Constructor given in comment

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/ConstrAndMethodDiff.java
@@ -0,0 +1,21 @@
+package com.mycompany.java_practice_project.OOP;
+
+public class ConstrAndMethodDiff {
+    
+    /*
+     ________________________________________________________________________
+     *         Constructor                   |               Method         *                                           *
+     *_______________________________________|______________________________*
+     * Constructor is used to initialize     | Method is used to expose     *
+     * the State of an object                | Behavior of an object        *
+     *_______________________________________|______________________________*
+     * Constructor must not have return type | Method have must return type *
+     *_______________________________________|______________________________*
+     * Constructor is invoked implicitly     | Method is Invoke Explicitly  *
+     *_______________________________________|______________________________*
+     * Constructor name must be same as the  | Method name should not be    *
+     * class name.                           | same as the class name.      *
+     *_______________________________________|______________________________*
+     
+     */
+}
