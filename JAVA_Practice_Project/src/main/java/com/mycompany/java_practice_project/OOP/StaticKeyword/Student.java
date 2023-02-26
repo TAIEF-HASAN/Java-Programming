915067289a7fd70bbb37bb3166fa4ed0ba1dab56
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-26 13:01:50 +0600

    modified Student class

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/StaticKeyword/Student.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/StaticKeyword/Student.java
@@ -1,11 +1,13 @@
 package com.mycompany.java_practice_project.OOP.StaticKeyword;
 
 public class Student {
-   
+   //Which  variable declare in the Class is called instance
     String name;
     int id;
+    // which data type before place static is called static variable
     static String UniName = "Diu";
 
+    //which variable declare in the method is called local variable
     Student(String n, int i){
         name = n;
         id = i;
