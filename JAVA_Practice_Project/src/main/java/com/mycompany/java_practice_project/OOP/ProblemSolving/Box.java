b0b144234adef1d771f5a40b3cdc69d2db6e3095
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-26 14:38:35 +0600

    modified

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/ProblemSolving/Box.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/ProblemSolving/Box.java
@@ -1,5 +1,14 @@
 package com.mycompany.java_practice_project.OOP.ProblemSolving;
 
+
+/*
+ * To solved the problem knowledge required about class, object, variable, data type, methods, constructor
+ * Create a class called Box that include three pieces of information as instance variable- height, width and 
+ * depth(type double) of to box. your class should have constructor and initialize three instance variables. Provide
+ * a method displayVol that display the volume of two boxes suppose, the value of instance variables for the first
+ * boxes are (10, 10, 10) and second boxes are (20, 30, 10). write a test application named BoxVolume the demonstrate
+ * class box's capabilities. 
+ */
 public class Box {
     
 
