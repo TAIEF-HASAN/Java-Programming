a41241f2772d3569c80c8919845f18ac117231d8
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-27 12:20:55 +0600

    commenting of type TypeConversion

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/TypeConversion.java
@@ -0,0 +1,19 @@
+package com.mycompany.java_practice_project.OOP;
+
+public class TypeConversion {
+    
+    //double --> none; 3.5 not convert to 3
+    //float --> double;
+    //long --> float, double
+    //int --> long, float, double
+    //char --> int, long, float, double
+    //short --> int, long, float, double
+    //byte --> short, int, long, float, double
+    //boolean --> None(boolean values, are not considered to be number in java
+    public static void main(String[] args) {
+        //float a = 3.4f;
+
+        //double b = a;
+
+    }
+}
