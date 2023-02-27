78f96d8b78be11a8f896af1e0656923628b85d70
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-27 10:10:08 +0600

    modified CallByValueTest

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/CallByValue/CallByValueTest.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/CallByValue/CallByValueTest.java
@@ -7,6 +7,8 @@ public class CallByValueTest {
         int x = 10; //primitive value --> primitive value store in stack memory
         System.out.println("x before call: "+x);
 
+
+        //When a method call by primitive data is called call by value
         ob.change(x);//here x is the actual parameter
         System.out.println("x after call: "+x);
     }
