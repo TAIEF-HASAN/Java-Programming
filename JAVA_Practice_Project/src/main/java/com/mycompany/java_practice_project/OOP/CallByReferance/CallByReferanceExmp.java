4b5c1af5a027062cbedf1230a4f52d39cf36caa3
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-27 11:02:21 +0600

    modified CallByReferanceExmp, CallByRefferenceTest

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/CallByReferance/CallByReferanceExmp.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/CallByReferance/CallByReferanceExmp.java
@@ -3,7 +3,9 @@ package com.mycompany.java_practice_project.OOP.CallByReferance;
 public class CallByReferanceExmp {
     
     String name;
+
     void change(CallByReferanceExmp r2){
+    //change to the formal parameter does affect the Actual parameter
        r2.name = "Habil";
     }
 }
--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/CallByReferance/CallByRefferenceTest.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/CallByReferance/CallByRefferenceTest.java
@@ -4,10 +4,14 @@ public class CallByRefferenceTest {
     public static void main(String[] args) {
         
         CallByReferanceExmp r1 = new CallByReferanceExmp();
-        r1.name = "Kabil";
+        r1.name = "Kabil"; 
         System.out.println("Before calling: "+r1.name);
 
-        r1.change(r1);
+
+       /*
+        *if we call a method by passing a reference type of data (object, String) then it is known as call by reference.
+        */
+        r1.change(r1); //In call by reference original value get change.
         System.out.println("After Calling: "+r1.name);
     }
 }
