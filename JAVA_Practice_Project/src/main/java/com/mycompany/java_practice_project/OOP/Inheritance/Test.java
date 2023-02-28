d3d3fa294a52d6e7ff82cde69f67610156569fdd
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-28 11:55:36 +0600

    modified Inheritance Test

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/Inheritance/Test.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/Inheritance/Test.java
@@ -3,7 +3,14 @@ package com.mycompany.java_practice_project.OOP.Inheritance;
 public class Test {
     public static void main(String[] args) {
 
-        
+        /*
+         * 1. What is Inheritance?
+         * Inheritance can be defined as the process where one class get the properties(methods and fields) fo another.
+         * Why do we need Inheritance?
+         * 1.For code reusability
+         * 2.For method overriding
+         * 3.To implement parent-child relationship. 
+         */
         Teacher t = new Teacher();
         t.name = "hamid";
         t.age = 25;
