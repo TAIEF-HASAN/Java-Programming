a5c3413ce127b4e02f86e26f27346b1157e3e8ef
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-28 16:48:57 +0600

    Example of OverLoadingVSOverRiding

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/OverLoadingVSOverRiding/OverLoadingVsOverRidingDiff.java
@@ -0,0 +1,21 @@
+package com.mycompany.java_practice_project.OOP.OverLoadingVSOverRiding;
+
+public class OverLoadingVsOverRidingDiff {
+    
+       
+    /*
+     ____________________________________________________________________________________
+     *         Method overload               |               Method override            *                                           *
+     *_______________________________________|__________________________________________*
+     * 1. Parameter must be Different        | 1.Parameter must be Same                 *
+     * 2.It occurs within the same class     | 2.It occurs between the different class  *
+     *_______________________________________|__________________________________________*
+     * 3.Inheritance is not involved         | 3.Inheritance involved                   *
+     *_______________________________________|__________________________________________*
+     * 4.Return type may and may not be same | 4.Return type must be same               *
+     *_______________________________________|__________________________________________*
+     * 5.One method does not hide another    | 5.child method hide parents another      *
+     *_______________________________________|__________________________________________*
+     
+     */
+}
