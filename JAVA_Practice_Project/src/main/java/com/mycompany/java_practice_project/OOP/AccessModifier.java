bb48f86e41c61a1658eeeeb92230dce81bda8cd4
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-28 10:47:30 +0600

    example of AccessModifier modified SetAndGetMethod

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/AccessModifier.java
@@ -0,0 +1,12 @@
+package com.mycompany.java_practice_project.OOP;
+
+public class AccessModifier {
+    /*
+     * Four Types of Access modifier in java
+     * 1.private
+     * 2.default
+     * 3.protected
+     * 4.public
+     */
+    
+}
--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/Encapsulation/SetAndGetMethod.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/Encapsulation/SetAndGetMethod.java
@@ -1,7 +1,23 @@
 package com.mycompany.java_practice_project.OOP.Encapsulation;
 
 public class SetAndGetMethod {
-    private String name;
+    
+    /*
+     * Encapsulation is a process of
+     * 1. Packaging Variable and Methods into single unit.
+     * 2. Protecting data by declaring them as private.
+     * 
+     * How to do Encapsulation ?
+     * 1. Declare the variable as private.
+     * 2.Provide public set and get method to modify and get the variable value.
+     * 
+     * Benefit:
+     * 1.Provided data hiding 
+     * 2.Reusability
+     * 3.code can be modify without breaking the code
+     * 4.Maintainability: Hiding implementation details reduces complexity.
+     */
+    private String name; //Private data will be hidden from other classes and they can only be accessed through the methods of their current class. this is known as data hiding.
     private int age;
 
     public void setAge(int age) {
