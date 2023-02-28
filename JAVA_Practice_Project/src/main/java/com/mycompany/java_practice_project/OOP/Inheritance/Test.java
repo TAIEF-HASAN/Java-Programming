e7860d36947355e40fe8e0a421bfa323a70814f3
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-28 11:43:51 +0600

    Example of Inheritance

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/Inheritance/Test.java
@@ -0,0 +1,13 @@
+package com.mycompany.java_practice_project.OOP.Inheritance;
+
+public class Test {
+    public static void main(String[] args) {
+
+        
+        Teacher t = new Teacher();
+        t.name = "hamid";
+        t.age = 25;
+        t.qualification = "BSc in CSE";
+        t.display2();
+    }
+}
