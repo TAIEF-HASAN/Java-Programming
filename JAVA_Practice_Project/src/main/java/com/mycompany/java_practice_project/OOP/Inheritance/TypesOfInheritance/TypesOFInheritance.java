9b9c9092c18b5078f22a52e09df458f23f201b83
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-28 15:22:26 +0600

    Example of TypesOFInheritance

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/Inheritance/TypesOfInheritance/TypesOFInheritance.java
@@ -0,0 +1,12 @@
+package com.mycompany.java_practice_project.OOP.Inheritance.TypesOfInheritance;
+
+public class TypesOFInheritance {
+    
+    /*
+     * There are four types Inheritance in OOP language. (-->) This Symbol means Extends
+     * 1.Single Inheritance. Example: B-->A (B Extends A)
+     * 2.Multilevel Inheritance. Example: C--> B--> A
+     * 3.Hierarchical Inheritance. Example: B--> A, C--> A, D-->A 
+     * 4.Multiple Inheritance. Example: B--> A, C-->A, D-->B,C
+     */
+}
