8c7758e0bf595104ac6c8f5fcd16323ec2d80af7
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-28 16:26:24 +0600

    modified MethodOverRide

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/MethodOverRide/Teacher.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/MethodOverRide/Teacher.java
@@ -4,6 +4,15 @@ public class Teacher extends Person {
     
     String qualification;
     //Here method override the another class of method
+    /*
+     * The rules of method overriding
+     * 1.Method name, signature, parameter must be same
+     * 2.if a method can not be inherited, then it can not be overridden.
+     * 3.Static method can not be over ride. Because static method is bound to class, on the other hand method method is bound to object.
+     * 4. A method declare with final cannot be override.
+     * 5.Constructor cannot be override.
+     */
+
     void display(){
         System.out.println("Name: "+name);
         System.out.println("Age: "+age);
