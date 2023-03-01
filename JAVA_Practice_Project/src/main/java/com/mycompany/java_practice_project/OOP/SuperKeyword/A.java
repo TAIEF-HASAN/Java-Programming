f3eea723a90b83475c8ae8bfaccc743ff6250267
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-03-01 10:39:23 +0600

    Example of other super class constructor by call in subclass with super class

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/SuperKeyword/A.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/SuperKeyword/A.java
@@ -3,6 +3,10 @@ package com.mycompany.java_practice_project.OOP.SuperKeyword;
 public class A {
     int x = 10;
 
+    A(){
+        System.out.println("A's Constructor");
+    }
+
     void display(){
         System.out.println("hI");
     }
--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/SuperKeyword/B.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/SuperKeyword/B.java
@@ -3,6 +3,11 @@ package com.mycompany.java_practice_project.OOP.SuperKeyword;
 public class B extends A {
     int x = 20;
 
+    B(){
+        super(); //for calling a constructor of other class's
+        System.out.println("B's constructor");
+    }
+
     //In Class B Always Priority for own class, For super class need to access with super dot method or variable name.
     void display(){
         super.display(); //for calling the display method of superClass need to add super keyworf dot display 
