c7311d853dec48684fbaa41fe2d16a17ecdf8df9
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-03-02 11:38:34 +0600

    modified

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/PolymorphismTw/Test.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/PolymorphismTw/Test.java
@@ -7,11 +7,12 @@ public class Test {
         Shape[] s = new Shape[3];
 
         s[0] = new Shape();
-        System.out.println(s[0].area()); 
-        s[1] = new Rectangle(10, 30);
-        System.out.println(s[1].area());
+        
         s[1] = new Rectangle(10, 30);
         
+        s[2] = new Triangle(10, 30);
+        System.out.println(s[0].area()); 
+        System.out.println(s[1].area());
         System.out.println(s[2].area());
         
     }
