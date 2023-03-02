74c63e0569c144b2354a360fa9f1ec924befabe6
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-03-02 12:54:40 +0600

    Example of Abstraction

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/OOP/Abstraction/Test.java
@@ -0,0 +1,18 @@
+package com.mycompany.java_practice_project.OOP.Abstraction;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+        
+        
+        MobileUser mobileUser;
+        mobileUser = new Rahim();
+        mobileUser.sendMessage();
+
+        mobileUser = new Karim();
+        mobileUser.sendMessage();
+       
+        
+    }
+}
