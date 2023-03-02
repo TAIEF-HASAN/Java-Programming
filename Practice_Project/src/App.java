ea9fbfa2663387408c612723e2698d806056bb73
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-03-02 23:18:50 +0600

    modified

--- Practice_Project/src/App.java
+++ Practice_Project/src/App.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Scanner n = new Scanner(System.in);
+        int num;
+        boolean b = false;
+        System.out.println("Enter number");
+        num = n.nextInt();
+       
+
+        for(int i=2; i<num;i++){
+            if(num%i==0){
+                b = true;
+                break;
+            }
+
+        }
+        if (b == true){
+            System.out.println("num is not prime");
+        }else{
+            System.out.println("prime");
+        }
+
     }
 }
