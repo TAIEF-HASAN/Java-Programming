c0745ab0ec404aaf553d6048338bc832bacf0348
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-03-05 15:07:42 +0600

    add example of Packages

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/Package/Cat.java
@@ -0,0 +1,8 @@
+package com.mycompany.java_practice_project.Package;
+
+public class Cat {
+    public static void main(String[] args) {
+        
+        
+    }
+}
---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/Package/Dog.java
@@ -0,0 +1,10 @@
+package com.mycompany.java_practice_project.Package;
+
+//import java.util.*; We can call a package like this, (*) means all
+import java.util.Scanner; // we can call a package like this. it means we need here Scanner class for input. So we not need to import all class form java.util package
+public class Dog {
+    //We can import package like this
+    //java.util.Scanner n = new java.util.Scanner(System.in);
+    Scanner n = new Scanner(System.in);
+
+}
