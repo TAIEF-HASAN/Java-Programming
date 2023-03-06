01a374d18a3945c33ba34dbbf2cc0984c2cef6b2
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-03-06 16:18:29 +0600

    modified ExceptionHandlingProblemSolving

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/ExceptionHandling/ExceptionHandlingProblemSolving.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/ExceptionHandling/ExceptionHandlingProblemSolving.java
@@ -1,5 +1,27 @@
 package com.mycompany.java_practice_project.ExceptionHandling;
 
+import java.util.Scanner;
+
 public class ExceptionHandlingProblemSolving {
     
+    public static void main(String[] args) {
+        
+        try {
+            Scanner n = new Scanner(System.in);
+            int x,y;
+            System.out.print("Please enter the num1: ");
+            x = n.nextInt();
+            System.out.print("Please enter the num2: ");
+            y = n.nextInt();
+
+            int result = x/y;
+            System.out.println("Result: "+result);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Exception: "+e);
+        }finally{
+            System.out.println("Please try again");
+        }
+    }
 }
