a08b81690dc0a0f34fde2b02ed6ebb7bcc8c021c
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-03-06 16:39:18 +0600

    modified ExceptionHandlingProblemSolving

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/ExceptionHandling/ExceptionHandlingProblemSolving.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/ExceptionHandling/ExceptionHandlingProblemSolving.java
@@ -5,23 +5,29 @@ import java.util.Scanner;
 public class ExceptionHandlingProblemSolving {
     
     public static void main(String[] args) {
-        
-        try {
-            Scanner n = new Scanner(System.in);
-            int x,y;
-            System.out.print("Please enter the num1: ");
-            x = n.nextInt();
-            System.out.print("Please enter the num2: ");
-            y = n.nextInt();
 
-            int result = x/y;
-            System.out.println("Result: "+x+"/"+y+" = "+result);
-
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.out.println("Exception: "+e);
-        }finally{
-            System.out.println("Please try again");
+        boolean b = false;
+        while (b!=true) {
+            try {
+                Scanner n = new Scanner(System.in);
+                int x,y;
+                System.out.print("Please enter the num1: ");
+                x = n.nextInt();
+                System.out.print("Please enter the num2: ");
+                y = n.nextInt();
+    
+                int result = x/y;
+                System.out.println("Result: "+x+"/"+y+" = "+result);
+                b = true;
+                break;
+    
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.out.println("Exception: "+e);
+                System.out.println("Please try again");
+            }
         }
+        
+        
     }
 }
