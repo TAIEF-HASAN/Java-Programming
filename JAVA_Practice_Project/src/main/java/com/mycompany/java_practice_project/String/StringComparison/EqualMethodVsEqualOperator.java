1518b8374fb9b0b9b2d998a68ab25b6da6f628f6
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-03-07 12:32:19 +0600

    added example of EqualMethodVsEqualOperator

---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/String/StringComparison/EqualMethodVsEqualOperator.java
@@ -0,0 +1,25 @@
+package com.mycompany.java_practice_project.String.StringComparison;
+
+public class EqualMethodVsEqualOperator {
+    //For object comparison equals() is better
+    //For primitive data type comparison == operator is better
+    public static void main(String[] args) {
+        String p1 = "Jamil123";
+        String p2 = "Jamil123";
+        String p3 = new String("Jamil123");
+        String p4 = new String("Jamil123");
+
+        //the comparison of == operator
+        System.out.println(p1==p2);
+        System.out.println(p1==p3);
+        System.out.println(p3==p4);
+        System.out.println();
+
+        //the comparison of equals()
+        System.out.println(p1.equals(p2));
+        System.out.println(p1.equals(p3));
+        System.out.println(p3.equals(p4));
+    }
+
+    
+}
---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/String/StringComparison/readme.md
@@ -0,0 +1,3 @@
+Main Difference between == and equals in Java is that "==" used to compare primitives while equal() method is recommended to check equality of objects value;
+
+equals() method compare to value of string.
