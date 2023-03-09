802ce5774f5948449e195ec78c14d6ea7873d4f3
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-03-09 14:03:08 +0600

    added new directory HashSet modified HashMap

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/HashMap/HashMapExample.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/HashMap/HashMapExample.java
@@ -5,16 +5,19 @@ import java.util.HashMap;
 public class HashMapExample {
     public static void main(String[] args) {
 
+        //hashMap depends on key value
         HashMap <Integer, String> sName = new HashMap<>();
 
         sName.put(101, "Ridy");
         sName.put(102, "Raj");
         sName.put(103, "Habil");
         sName.put(104, "Kabil");
+        sName.put(104,"kabil");
         
         System.out.println(sName.get(101));
         System.out.println(sName.get(102));
         System.out.println(sName.get(103));
+        System.out.println(sName.get(104));
         System.out.println(sName.get(104));
     }
 }
---
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/HashSet/HashSetExample.java
@@ -0,0 +1,23 @@
+package com.mycompany.java_practice_project.HashSet;
+
+import java.util.HashSet;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        
+        //set can not take duplicate value
+        HashSet<String> fruitsName = new HashSet<>();
+
+        fruitsName.add("apple");
+        fruitsName.add("banana");
+        fruitsName.add("bit");
+        fruitsName.add("jackfruit");
+        fruitsName.add("pineapple");
+        fruitsName.add("bit");
+
+
+
+
+        System.out.println(fruitsName);
+    }
+}
