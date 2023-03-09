a70224eb134cd3761135bd17fb747fbe96434149
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-03-09 10:34:33 +0600

    modified LinkedListExample

--- JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/List/LinkedListExample.java
+++ JAVA_Practice_Project/src/main/java/com/mycompany/java_practice_project/List/LinkedListExample.java
@@ -13,7 +13,21 @@ public class LinkedListExample {
         name.add("India");
         name.add("Nepal");
         name.add("Vutan");
+        name.add(5, "Hawaii");
+        name.addFirst("Australia");
+        name.addLast("Japan");
+        //name.remove("India");
+        //name.remove(0);
+        //name.removeFirst();
+        //name.removeLast();
+        //name.removeAll(name);
 
+        for (String namString : name) {
+            System.out.println(namString);
+        }
+
+        //System.out.println(name);
+        name.clear();
         System.out.println(name);
     }
 }
