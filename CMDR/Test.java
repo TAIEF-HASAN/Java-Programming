bc2dc0cbe59662ce9b44a7c063b491c1cb554394
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-03-12 18:05:56 +0600

    modified

--- CMDR/Test.java
+++ CMDR/Test.java
@@ -1,9 +1,23 @@
+package CMDR;
+
+import java.util.Scanner;
+
 class Test{
 	
 	public static void main(String[]args){
 		
-		System.out.println("HEllo World");
-	
+		Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        int i = scan.nextInt();
+
+        // Write your code here.
+        
+        //double d = scan.nextDouble();
+       
+
+        System.out.println("String: " + s);
+        //System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
 	}
 
 }
